44df7100064e03f35c2a588e42a47b856b681108
Matt Lamontagne <lemontongs@gmail.com>
2014-11-05 07:29:10 -0500

    user thread initial commit

--- spark_thermostat_html_builder.py
+++ spark_thermostat_html_builder.py
@@ -5,10 +5,11 @@ import os
 import sys
 import time
 import temperature_thread
+import user_thread
 
 DEBUG = False
 
-def build_html_file(filename, thermostat):
+def build_html_file(filename, thermostat, user_thread):
 
     if DEBUG:
         print "building file"
@@ -17,7 +18,10 @@ def build_html_file(filename, thermostat):
     content = f.read()
     f.close()
     
-    content = content % (thermostat.get_history(), thermostat.get_temp())
+    content = content % (thermostat.get_history(),  \
+                         user_thread.get_history(), \
+                         thermostat.get_temp(),     \
+                         user_thread.get_is_someone_home())
     
     
     if DEBUG:
@@ -52,27 +56,57 @@ if __name__ == '__main__':
     
     filename = "/var/www/thermostat.html"
     
+    if len(sys.argv) == 2:
+        if "-h" == sys.argv[1]:
+            print "Usage: python %s [html output file]" % sys.argv[0]
+            os._exit(0)
+        
+        filename = sys.argv[1]
+    
+    
     check_permissions(filename)
     
+    ############################################################################
+    # Thermostat Threat
+    ############################################################################
     thermostat1 = \
         temperature_thread.Temperature_Thread(filename="floor_temps.csv", 
                                               device_names=["main_floor_temp",
                                                             "top_floor_temp",
                                                             "basement_floor_temp"])
     if thermostat1 == None:
-        print "Error creating thread"
+        print "Error creating temperature thread"
         sys.exit(1)
     
     thermostat1.start()
 
+    ############################################################################
+    # User Threat
+    ############################################################################
+    user1 = \
+        user_thread.User_Thread(filename = "user_state.csv", 
+                                users = [("Matt","bc:f5:ac:f4:35:95"),
+                                         ("Kat", "58:a2:b5:e9:2b:fc"),
+                                         ("Adam","24:e3:14:d2:f8:b2")])
+    if user1 == None:
+        print "Error creating user thread"
+        thermostat1.stop()
+        sys.exit(1)
+    
+    user1.start()
+    
+    ############################################################################
+    # Main loop
+    ############################################################################
     while True:
         try:
-            build_html_file(filename, thermostat1)
+            build_html_file(filename, thermostat1, user1)
             os.system("/home/mlamonta/bin/blink1-tool -q --hsb=130,200,50")
             time.sleep(60)
             os.system("/home/mlamonta/bin/blink1-tool -q --off")
         except KeyboardInterrupt:
             thermostat1.stop()
+            user1.stop()
             os._exit(0)
         
 
