9a062f2b0b4b2f3e55371fb96a723f5229dbcb18
Matt Lamontagne <lemontongs@gmail.com>
2014-12-09 21:19:19 -0500

    added submit button to webpage

---
+++ cgi-bin/set_temp.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import sys
+import cgi
+import zmq
+
+import cgitb
+cgitb.enable()
+
+def send_temperature(set_point,floor):
+  try:
+    context = zmq.Context()
+    socket = context.socket(zmq.PAIR)
+    socket.setsockopt(zmq.LINGER, 2000)
+    socket.connect("tcp://localhost:5555")
+    socket.send("set_point,%s,%s"%(floor,str(set_point)))
+  except:
+    print sys.exc_info()
+
+
+# print header
+print "Content-type: text/html\n\n"
+
+form = cgi.FieldStorage()
+
+if ('set_temp' in form) and ('floor' in form):
+  try:
+    set_point = float(form['set_temp'].value)
+    floor = form['floor'].value
+    
+    if set_point > 80:
+      print "set point too high"
+    elif set_point < 50:
+      print "set point too low"
+    elif (floor != "top_floor_temp") and (floor != "basement_floor_temp") and (floor != "main_floor_temp"):
+      print "invalid floor:", floor
+    else:
+      print "OK"
+      send_temperature(set_point,floor)
+  except:
+    print "bad temp", sys.exc_info()
+
